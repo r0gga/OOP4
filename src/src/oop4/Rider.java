1587aff58c10b52c429be1d4d473a3e0f8c215d5
rogga <oskaroetken@web.de>
2024-05-13 15:06:51 +0200

    Add constructor and attributes

--- src/src/oop4/Rider.java
+++ src/src/oop4/Rider.java
@@ -1,4 +1,15 @@
 package oop4;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Rider {
+
+    private String name;
+    private HashSet<Horse> preferences;
+
+    public Rider(String name, Set<Horse> preferences){
+        this.name = name;
+        this.preferences = new HashSet<>(preferences);
+    }
 }
