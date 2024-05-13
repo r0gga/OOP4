83d72108fcae90c4ba1967e7efa89c1826830d03
rogga <oskaroetken@web.de>
2024-05-13 15:04:51 +0200

    Add constructor and attributes

--- src/src/oop4/Horse.java
+++ src/src/oop4/Horse.java
@@ -1,4 +1,12 @@
 package oop4;
 
 public class Horse {
+
+    private String name;
+    private String skill;
+
+    private Horse(String name, String skill){
+        this.name = name;
+        this.skill = skill;
+    }
 }
