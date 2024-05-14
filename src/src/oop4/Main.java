04fc1b70d8e4bf66d22ad286c4d80093f29336cc
ARCTA\Richm <RichmondNovaTactical@outlook.de>
2024-05-14 10:10:22 +0200

    bissl ideen rumprobieren

--- src/src/oop4/Main.java
+++ src/src/oop4/Main.java
@@ -1,7 +1,13 @@
 package oop4;
 
+import java.util.HashMap;
+import java.util.HashSet;
 
 public class Main {
     public static void main(String[] args) {
+        //bissl ideen rumprobieren:
+        HashMap<Rider, Horse> mapping = new HashMap<>();
+        
+
     }
 }
