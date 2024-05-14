2407b22c79ec1cd97040339db8b3546b8b484e2b
rogga <oskaroetken@web.de>
2024-05-14 12:01:19 +0200

    Implement findBestMatches method

--- src/src/oop4/RiderHorseMatching.java
+++ src/src/oop4/RiderHorseMatching.java
@@ -9,13 +9,13 @@ public class RiderHorseMatching {
     private List<Rider> riders;
     private List<Horse> horses;
     private Map<Rider, Horse> bestMatches;
-    private int maxMatches;
+    private int maxWishes;
 
     public RiderHorseMatching(List<Rider> riders, List<Horse> horses){
         this.riders = riders;
         this.horses = horses;
         bestMatches = new HashMap<>();
-        maxMatches = 0;
+        maxWishes = 0;
     }
 
     // match prüft, ob ein Reiter und ein Pferd eine gültige Kommbination bilden
@@ -41,9 +41,31 @@ public class RiderHorseMatching {
 
     // Map, die die aktuellen Matches speichert, für findBestMatches Methode
 
-    Map<Horse, Rider> currentMatches = new HashMap<>();
+    Map<Horse, Rider> currentAsignments = new HashMap<>();
     //
     public void findBestMatches(int index, Map<Rider, Horse> currentMatches, boolean[] horseOccupied){
+        if(index == riders.size()){
+            int fulfilledWishes = calculateMatches(currentMatches);
+            if(fulfilledWishes > maxWishes){
+                maxWishes = fulfilledWishes;
+                bestMatches = currentMatches;
+            }
+            return;
+        }
+        Rider currentRider = riders.get(index);
+        for(int i = 0; i < horses.size(); i++){
+            Horse currentHorse = horses.get(i);
+            if(!horseOccupied[i] && match(currentRider, currentHorse)){
+                currentMatches.put(currentRider, currentHorse);
+                horseOccupied[i] = true;
+
+                findBestMatches(index + 1, currentMatches, horseOccupied);
+
+                currentMatches.remove(currentRider);
+                horseOccupied[i] = false;
+            }
+        }
+
 
     }
 
