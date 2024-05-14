953622e38546d0314879d96e95b43d7b392f5c0c
ARCTA\Richm <RichmondNovaTactical@outlook.de>
2024-05-14 10:53:39 +0200

    final private im attribut

--- src/src/oop4/Rider.java
+++ src/src/oop4/Rider.java
@@ -5,7 +5,7 @@ import java.util.Set;
 
 public class Rider extends StableMember{
 
-    private Set<Horse> preferences;
+    final private Set<Horse> preferences;
 
 
     public Rider(String name, int characteristic, Set<Horse> preferences){
