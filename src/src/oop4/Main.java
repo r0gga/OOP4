d089ee384d72a6a73c340e8fe5c51c2712fc9cca
rogga <oskaroetken@web.de>
2024-05-14 10:28:09 +0200

    Add horses

--- src/src/oop4/Main.java
+++ src/src/oop4/Main.java
@@ -7,7 +7,19 @@ public class Main {
     public static void main(String[] args) {
         //bissl ideen rumprobieren:
         HashMap<Rider, Horse> mapping = new HashMap<>();
-        
+
+
+
+        Horse horse1 = new Horse("Alex", 0);
+        Horse horse2 = new Horse("Nicki", 1);
+        Horse horse3 = new Horse("Pucki", 0);
+        Horse horse4 = new Horse("Wittchen", 0);
+        Horse horse5 = new Horse("Hurrikan", 1);
+        Horse horse6 = new Horse("Tornado", 2);
+        Horse horse7 = new Horse("Sturmwind", 1);
+        Horse horse8 = new Horse("Zausel", 0);
+        Horse horse9 = new Horse("Zackzack", 2);
+
 
     }
 }
