add9448fc93f957ca400bbeba8830aeb9097565d
ARCTA\Richm <RichmondNovaTactical@outlook.de>
2024-05-14 10:34:02 +0200

    mal alle probedaten geprinted

--- src/src/oop4/Main.java
+++ src/src/oop4/Main.java
@@ -7,6 +7,22 @@ public class Main {
     public static void main(String[] args) {
         //bissl ideen rumprobieren:
         HashMap<Rider, Horse> mapping = new HashMap<>();
+        Stable s1 = new Stable();
+
+        System.out.println("Reiter: ");
+        for(Rider r : s1.getRiders()){
+            System.out.print(r.getName() +" " + r.getCharacteristic() + " ");
+            for(Horse h : r.getPreferences()){
+                System.out.print(h.getName() + ", ");
+            }
+            System.out.println();
+        }
+
+        System.out.println("\nPferde: ");
+        for(Horse h : s1.getHorses()){
+            System.out.println(h.getName()+ " " + h.getCharacteristic());
+        }
+       
         
 
     }
