8813ab55b9e57ebd9395b97aa3c4fa1ca7e58940
rogga <oskaroetken@web.de>
2024-06-02 23:45:49 +0200

    Add Javadoc

--- src/src/oop4/Rider.java
+++ src/src/oop4/Rider.java
@@ -2,17 +2,35 @@ package oop4;
 
 import java.util.HashSet;
 import java.util.Set;
+/**
+ * The Rider class represents a rider in a stable.
+ * It extends the StableMember class and includes additional information about the rider's horse preferences.
+ */
 
 public class Rider extends StableMember{
 
+    /**
+     * The set of horses that the rider prefers.
+     */
     final private Set<Horse> PREFERENCES;
 
-
+    /**
+     * Constructs a new Rider instance with the specified name, characteristic, and preferences.
+     *
+     * @param name The name of the rider.
+     * @param characteristic The characteristic level of the rider (0 = Anfänger, 1 = gute Reiter, 2 = sehr gute Reiter).
+     * @param preferences The set of horses that the rider prefers.
+     */
     public Rider(String name, int characteristic, Set<Horse> preferences){
         super(name, characteristic);
         this.PREFERENCES = new HashSet<>(preferences);
     }
 
+    /**
+     * Gets the set of horses that the rider prefers.
+     *
+     * @return The set of preferred horses.
+     */
     public Set<Horse> getPreferences(){
         return this.PREFERENCES;
     }
