cf2a843df9eaf6c2e6b48ef48abe2a8f69e1e460
rogga <oskaroetken@web.de>
2024-06-02 23:46:11 +0200

    Add Javadoc

--- src/src/oop4/Horse.java
+++ src/src/oop4/Horse.java
@@ -1,7 +1,17 @@
 package oop4;
 
+/**
+ * The Horse class represents a horse in a stable.
+ * It extends the StableMember class and includes information about the horse's characteristic level.
+ */
 public class Horse extends StableMember{
 
+    /**
+     * Constructs a new Horse instance with the specified name and characteristic.
+     *
+     * @param name The name of the horse.
+     * @param characteristic The characteristic level of the horse (0 = Anfänger, 1 = gute Reiter, 2 = sehr gute Reiter).
+     */
  // 0 = Anfänger, 1 = gute Reiter, 2 = sehr gute Reiter
     public Horse(String name, int characteristic){
         super(name, characteristic);  
